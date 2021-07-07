fc45cc00f6b0701f8698350760ac930c7d87def4
Sonny Alves Dias <sonnyad.contact@gmail.com>
2021-07-08 00:09:10 +0800

    Adds a deploy script to update the remote nginx config, and updates the web config to add the front end

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh server rm /etc/nginx/conf.d/.utile.bak
+ssh server mv /etc/nginx/conf.d/utile.conf /etc/nginx/conf.d/.utile.bak
+scp utile.conf server:/etc/nginx/conf.d/utile.conf
+(ssh server "nginx -t && service nginx reload" && echo "Deployed") || echo "Not deployed!"
