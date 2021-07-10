5c1328946adb5bb43b5500c834d350264eebb6cb
Sonny Alves Dias <sonnyad.contact@gmail.com>
2021-07-11 00:16:52 +0800

    Improves the deploy script to be faster

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ssh server rm /etc/nginx/conf.d/.utile.bak
-ssh server mv /etc/nginx/conf.d/utile.conf /etc/nginx/conf.d/.utile.bak
+ssh server "
+    rm /etc/nginx/conf.d/.utile.bak
+    mv /etc/nginx/conf.d/utile.conf /etc/nginx/conf.d/.utile.bak
+"
 scp utile.conf server:/etc/nginx/conf.d/utile.conf
 (ssh server "nginx -t && service nginx reload" && echo "Deployed") || echo "Not deployed!"
